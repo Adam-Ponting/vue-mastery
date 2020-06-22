9e3a8f504eed82a2d380c09fd079894257192720
Adam Ponting <adam.ponting@gmail.com>
2020-06-22 14:51:43 +0100

    done 4

--- intro-to-vue/main.js
+++ intro-to-vue/main.js
@@ -10,5 +10,17 @@ var app = new Vue({
     viewMore: 'https://www.amazon.co.uk/Mens-Socks/b?node=1731008031',
     inventory: 9,
     onSale: true,
+    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: 'green',
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+      },
+    ],
+    sizes: ['xs', 's', 'm', 'l', 'xl'],
   },
 });
